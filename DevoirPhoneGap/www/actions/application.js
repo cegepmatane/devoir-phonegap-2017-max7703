194c964ef1b27e2c98df761cbcfa5594fcd3d3ef
max7703 <max7703@live.fr>
2017-10-27 13:28:34 -0400

    liste de cadeau qui s'affiche sans DAO

--- DevoirPhoneGap/www/actions/application.js
+++ DevoirPhoneGap/www/actions/application.js
@@ -1,1 +1,19 @@
-﻿
+﻿var applicationListeNoel = {
+    lancer: function ()
+    {
+        this.naviguer();
+    },
+
+    naviguer: function ()
+    {
+        var ancre = window.location.hash;
+        if (!ancre)
+        {
+            var listeCadeauVue = new ListeCadeauVue();
+
+            listeCadeauVue.afficher();
+        }
+    }
+}
+
+applicationListeNoel.lancer();
---
+++ DevoirPhoneGap/www/actions/vues/ListeCadeauxVue.js
@@ -0,0 +1,23 @@
+﻿var ListeCadeauVue = function () {
+
+    this.afficher = function () {
+        var pageListeCadeau = $("#page-liste-cadeau").html();
+
+        $("body").html(pageListeCadeau);
+
+        var htmlListeCadeau = $("#liste-cadeau");
+
+        var listeCadeau = [
+            { "id": 1, "nom": "Pebble", "marque": "Pebble", "description": "Montre intel" },
+            { "id": 2, "nom": "iPhone", "marque": "Apple", "description": "telephone" }
+        ];
+
+        var li = "";
+
+        for (var indiceCadeau in listeCadeau) {
+            li += '<li>' + listeCadeau[indiceCadeau].nom + '</li>';
+        }
+
+        htmlListeCadeau.html(li);
+    }
+}
