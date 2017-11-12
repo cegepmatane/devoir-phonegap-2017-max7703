7a2c9b1cedb00a7d9667b7ee228d1f9cda774534
max7703 <max7703@live.fr>
2017-11-12 14:51:50 -0500

    ajout de la vue ajouter un cadeau

---
+++ DevoirPhoneGap/www/actions/vues/AjouterCadeauVue.js
@@ -0,0 +1,21 @@
+﻿var AjouterCadeauVue = function () {
+    var instance = this;
+    this.cadeau = null;
+    this.afficher = function () {
+        this.cadeau = null;
+
+        $("body").html(AjouterCadeauVue.pageAjouterCadeau);
+
+        $("#formulaire-ajouter").on("submit", function (evenement) {
+            evenement.preventDefault();
+            var nom = $("#nom").val();
+            var marque = $("#marque").val();
+            var prix = $("#prix").val();
+            var description = $("#description").val();
+
+            instance.cadeau = new Cadeau(nom, marque, prix, description)
+            window.location.hash = "#AjouterCadeauVue:NouveauCadeau";
+        });
+    }
+}
+AjouterCadeauVue.pageAjouterCadeau = $("#page-ajouter-cadeau").html();
