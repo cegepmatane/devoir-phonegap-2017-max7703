75e2967442c78d03ad3791d15a82d3dfb1148aa8
max7703 <max7703@live.fr>
2017-11-12 14:42:48 -0500

    ajout du modele "cadeau"

---
+++ DevoirPhoneGap/www/actions/modeles/cadeau.js
@@ -0,0 +1,7 @@
+﻿var Cadeau = function (nom, marque, prix, description, id = undefined) {
+    this.id = id;
+    this.nom = nom;
+    this.marque = marque;
+    this.prix = prix;
+    this.description = description;
+}
